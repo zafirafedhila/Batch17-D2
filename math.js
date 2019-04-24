33cea863fc359e355b88c421b2f21dcc774f90a7
ZAF <zafirafedhila@users.noreply.github.com>
2019-04-24 21:48:51 +0700

    Add files via upload

---
+++ math.js
@@ -0,0 +1,36 @@
+var angkaPi=Math.PI
+var pangkatDua=Math.pow(4,2)
+var angkaAcak= Math.random()
+var angkaEuler=Math.Euler
+var ln2 = Math.LN2
+var ln10 = Math.LN10
+var log2e=Math.LOG2E
+var log10e=Math.LOG10E
+var sqrt1_2= Math.SQRT1_2
+var sqrt2=Math.SQRT2
+
+console.log(angkaPi);
+console.log(pangkatDua);
+console.log(angkaAcak);
+console.log(angkaEuler);
+console.log(ln2);
+console.log(ln10);
+console.log(log2e);
+console.log(log10e);
+console.log(sqrt1_2);
+console.log(sqrt2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
